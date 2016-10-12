4e10480523b74adf7c0c61e8bbbf580f32359421
BitCamp <BitCamp@BitCamp-PC>
2016-10-12 09:20:38 +0900

    .

---
+++ Project/src/main/webapp/travel/js/memberForm.js
@@ -0,0 +1,115 @@
+$("#signInBtn").click(function(event) {
+
+	var member = {
+			name: $("#name").val(),
+			email: $(".col-sm-10 > #email").val(),
+			password: $(".col-sm-10 > #password").val(),
+			birthday: $("#birthday").val(),
+			gender: $("#gender").val()
+
+	}
+	console.log(member)
+	var count = 0;
+	for (var key in member) {
+		if (member[key] != "") {
+			console.log(member[key])
+			count++
+		}
+	}
+	console.log(count)
+	if (count == 5) {
+		ajaxAddMember(member)
+		alert("등록 성공입니다.")
+		window.location.reload(true)
+	} else {
+	alert("등록 실패입니다.")
+	}
+});
+
+
+
+$("#updateBtn").click(function(event) {
+
+	var member = {
+			name: $("#name").val(),
+			email: $("#email").val(),
+			password: $("#password").val(),
+			birthday: $("#birthday").val(),
+			gender: $("#gender").val(),
+			registedDate: $("#registedDate").val(),
+			no: $("#no").val()
+	}
+	ajaxUpdateMember(member)
+});
+
+$("#deleteBtn").click(function(event) {
+
+	ajaxDeleteMember($("#no").val(),
+			$("#password").val())
+});
+
+function ajaxAddMember(member) {
+
+	$.post("add.json", member, function(obj) {
+		var result = obj.jsonResult
+		if (result.state != "success") {
+			console.log(result.data)
+			alert("등록 실패입니다.")
+			return
+		}
+
+	}, "json")
+}
+
+function ajaxLoadMember(no) {
+
+	$.getJSON("detail.json?no="+ no, function(obj) {
+		var result = obj.jsonResult
+		if (result.state != "success") {
+			console.log(result.data)
+			alert("조회 실패입니다.")
+			return;
+		}
+
+		$("#no").val(result.data.no);
+		$("#name").val(result.data.name);
+		$("#email").val(result.data.email);
+		$("#birthday").val(result.data.birthday);
+		$("#gender").val(result.data.gender);
+		$("#registedDate").val(result.data.registedDate);
+		$("#memberPhoto").text(result.data.memberPhoto);
+	})
+}
+
+function ajaxUpdateMember(member) {
+	console.log(member)
+
+	$.post("update.json", member, function(obj) {
+		var result = obj.jsonResult
+		console.log(member)
+		if (result.state != "success") {
+			console.log(result.data)
+			alert("변경 실패입니다.")
+			return
+		}
+		window.location.href = "memberApp.html"
+	}, "json")
+}
+
+
+function ajaxDeleteMember(no, password) {
+
+	$.getJSON("delete.json", {
+		no: no, 
+		password: password
+	}, function(obj) {
+		var result = obj.jsonResult
+		if (result.state != "success") {
+			console.log(result.data)
+			alert("삭제 실패입니다.")
+			return
+		}
+		window.location.href = "memberApp.html"
+	})
+
+}
