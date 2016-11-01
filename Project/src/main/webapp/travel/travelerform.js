4bc387a197e9489c2d8bf858ccf773d968211a58
chan <chan@DESKTOP-O7KL2CB>
2016-11-01 19:29:54 +0900

    .

--- Project/src/main/webapp/travel/travelerform.js
+++ Project/src/main/webapp/travel/travelerform.js
@@ -192,6 +192,7 @@ function ajaxLoadTravelMain(no) {
 		$("#selfIntroduce").val(result.data.selfIntroduce);
 		$("#selfIntroduce").text(result.data.selfIntroduce);
 		$("#styleName").val(result.data.styleNo);
+		("#detailUserImage").attr("src","../upload/" + result.data.myPhoto);
 
 		favorChecked(result)
 		scheduleList()
