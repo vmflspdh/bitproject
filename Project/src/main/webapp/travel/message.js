613eaf90e42594c6e45e331b06617d63472baae7
chan <chan@DESKTOP-O7KL2CB>
2016-11-03 13:54:25 +0900

    메세지 수정-양찬

--- Project/src/main/webapp/travel/message.js
+++ Project/src/main/webapp/travel/message.js
@@ -48,13 +48,19 @@ function chattingMemberList() {
 	      }
 	    }
 	    $(".chanchatting").html(contents)
+	    loadDetailMessageList(arr[0].sendMemberNo)
+	    loadProfileInfo(arr[0].sendMemberNo)
+	    $('#detailpageContents').css("display", "inherit")
+	    $('#mainMessageBtn').css("display", "inherit")
 	    $(".titleLink").click(function(event) {
 	    	var no = $(this).attr("data-sendMemberNo")
 	    	loadDetailMessageList(no)
+	    	loadProfileInfo(no)
+	        $('#detailpageContents').css("display", "inherit")
+	        $('#mainMessageBtn').css("display", "inherit")
 	    })
     })
 }
-
 function loadDetailMessageList(no) {
 	$.getJSON(serverAddr + "/travel/myMessageList.json", {no: no}, function(obj) {
 		var result = obj.jsonResult
@@ -65,30 +71,88 @@ function loadDetailMessageList(no) {
 		var contents = "";
 		var arr = result.data
 		for (var i in arr) {
-			if (arr[i].sendMemberNo == no) {
+			if (arr[i].sendMemberNo != no) {
+				if (i > 0 && arr[i - 1].sendMemberNo != no) {
+					contents +=
+						'<div class="chattingDetail">' +
+						'<div style="width:50px; height:50px; border-radius: 25px 25px 25px 25px; overflow: hidden; float: right;">' +
+						'</div>' +
+						'<div style="border:0px solid black; width: 280px; float: left; background-color: #F5F2F5; padding:10px; border-radius: 15px 15px 15px 15px;">' +
+						'<div>' + arr[i].contents + '</div>' +
+						'<div style="font-size: x-small;">' + arr[i].sendDate + '</div>' +
+						'</div>' +
+						'</div>'
+				} else {
+				contents +=
+					'<div class="chattingDetail">' +
+					'<div style="width:50px; height:50px; border-radius: 25px 25px 25px 25px; overflow: hidden; float: right;">' +
+					'<img src="../upload/' + arr[i].myPhoto + '" ></div>' +
+					'<div style="border:0px solid black; width: 280px; float: left; background-color: #F5F2F5; padding:10px; border-radius: 15px 15px 15px 15px;">' +
+					'<div>' + arr[i].contents + '</div>' +
+					'<div style="font-size: x-small;">' + arr[i].sendDate + '</div>' +
+					'</div>' +
+					'</div>'
+				}
+			} else {
+				if (i > 0 && arr[i - 1].sendMemberNo == no) {
+					contents +=
+						'<div class="chattingDetail">' +
+						'<div style="width:50px; height:50px; border-radius: 25px 25px 25px 25px; overflow: hidden; float: left;">' +
+						'</div>' +
+						'<div style="border:0px solid black; width:280px; float: right; background-color: #DCE6F2; padding:10px; border-radius: 15px 15px 15px 15px;">' +
+						'<div>' + arr[i].contents + '</div>' +
+						'<div style="font-size: x-small;">' + arr[i].sendDate + '</div>' +
+						'</div>' +
+						'</div>'
+				} else {
 				contents +=
 					'<div class="chattingDetail">' +
 					'<div style="width:50px; height:50px; border-radius: 25px 25px 25px 25px; overflow: hidden; float: left;">' +
-					'<img src="../upload/' + arr[i].myPhoto + '"></div>' +
+					'<img src="../upload/' + arr[i].myPhoto + '" ></div>' +
+					'<div style="border:0px solid black; width:280px; float: right; background-color: #DCE6F2; padding:10px; border-radius: 15px 15px 15px 15px;">' +
 					'<div>' + arr[i].contents + '</div>' +
 					'<div style="font-size: x-small;">' + arr[i].sendDate + '</div>' +
-					'</div>'
-			} else {
-				contents +=
-					'<div class="chattingDetail" style="background-color: white;">' +
-					'<div style="width:50px; height:50px; border-radius: 25px 25px 25px 25px; overflow: hidden; float: right;">' +
-					'<img src="../upload/' + arr[i].myPhoto + '"></div>' +
-					'<div>' + arr[i].contents + '</div>' +
-					'<div style="font-size: x-small;">' + arr[i].sendDate + '</div>' +
-					'</div>'
+					'</div>' +
+					'</div><br>'
+				}
 			}
-/*			if (i == arr.length - 1) {
-				contents +=
-					'<textarea id="detailpageContents" cols="50" rows="3" style="width: 400px;" placeholder="내용을 입력하세요"></textarea>' +
-					'<button id="mainMessageBtn" style="float:right;">입력</button>'
-			}*/
 		}
 		$(".chanchattingList").html(contents)
+		
+    })                  
+}
+
+function loadProfileInfo(no) {
+	$.getJSON(serverAddr + "/travel/profiledetail.json", {no: no}, function(obj) {
+		var result = obj.jsonResult
+		if (result.state != "success") {
+		       alert("서버에서 데이터를 가져오는데 실패했습니다.")
+		       return
+		}
+		var arr = result.data
+		var contents = 
+			'<div style="width:200px;">' +
+			'<div style="width:200px; height:200px; border-radius: 50% 50% 50% 50%; overflow: hidden;">' +
+			'<img src="../upload/' + arr[0].myPhoto + '" style="width:200px;">' +
+			'</div><br>' +
+			'<div style="font-size: large; font-weight: bold;"><center>' + arr[0].receiveUser + '</center></div><br>' +
+			'<div style="font-size: medium;"><center>' + arr[0].contents + '</center></div><br>' +
+			'<div style="font-size: medium;"><a class="travelMainLink" href="#" data-travelMainBoard=' + arr[0].schduleNo + '><center>상세페이지로 이동</center></a></div>' +
+			'</div>'
+		  
+		$(".chanProfile").html(contents)
+		$(".travelMainLink").click(function(event) {
+	    	var no = $(this).attr("data-travelMainBoard")
+	    	checkToNo(no)
+	    })
+    })                  
+}
+
+function checkToNo(no) {
+	$.getJSON(serverAddr + "/travel/formMyList.json", {no: no}, function(obj) {
+		var result = obj.jsonResult
+		
+		window.location.href = "newdetail.html?no=" + no
     })                  
 }
 
@@ -100,7 +164,7 @@ function ajaxAddMessage(message) {
 			return
 		}
 		
-
+		
 		window.location.reload(true)
 	}, "json")
 }
