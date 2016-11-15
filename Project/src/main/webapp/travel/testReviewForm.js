0dba93adab2970538eba4426af06f5cabf843301
BitCamp <BitCamp@BitCamp-PC>
2016-11-15 14:10:55 +0900

    .

--- Project/src/main/webapp/travel/testReviewForm.js
+++ Project/src/main/webapp/travel/testReviewForm.js
@@ -12,7 +12,7 @@ function ajaxReviewContentList(no) {
 			contents += 
 				'<li class="event"><input type="radio" name="tl-group" /> <label></label>'+
 				'<div class="thumb user-2">'+
-											'<img width=100% height=100% src="../upload/'+arr[i].reviewBoardContentPhotoName+'" alt="...">'+
+		'<img width=100% height=100% src="../upload/'+arr[i].reviewBoardContentPhotoName+'" alt="...">'+
 				'</div>'+
 				'<div class="content-perspective">'+
 					'<div class="content">'+
@@ -29,7 +29,6 @@ function ajaxReviewContentList(no) {
 				'</div>'+
 				'<hr>'+
 				'</li>'
-
 		}
 		// html 원래 위치
 		$(".timeline").html(contents);
