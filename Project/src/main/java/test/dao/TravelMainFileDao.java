8c2b67c6bdd0dc2b0f5e3258b248898d1678982a
BitCamp <BitCamp@BitCamp-PC>
2016-11-20 19:16:17 +0900

    여행글 수정

--- Project/src/main/java/test/dao/TravelMainFileDao.java
+++ Project/src/main/java/test/dao/TravelMainFileDao.java
@@ -9,4 +9,5 @@ public interface TravelMainFileDao {
   int insert(TravelMainFile travelMainFile);
   List<TravelMainFile> selectList(Map<String,Object> paramMap) throws Exception;
   int delete(int no) throws Exception;
+  int update(TravelMainFile travelMainFile) throws Exception;
 }
